a1502f2036477a6bf7bbeeab4498e870808e6472
Srinidhijayaprakash <51017536+Srinidhijayaprakash@users.noreply.github.com>
2019-05-25 15:03:43 +0530

    Update signofnum.py

--- signofnum.py
+++ signofnum.py
@@ -1,7 +1,7 @@
 a=float(input())
 if(a>0):
-  print("THE NUMBER IS POSITIVE")
+  print("POSITIVE")
 elif(a<0):
-  print("THE NUMBER IS NEGATIVE")
+  print("NEGATIVE")
 else:
-   print("THE NUMBER IS 0")
+   print("ZERO")
