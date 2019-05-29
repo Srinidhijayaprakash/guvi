54baa9c4d563159eb01e7ff9e00707f580208efc
Srinidhijayaprakash <51017536+Srinidhijayaprakash@users.noreply.github.com>
2019-05-29 02:21:20 -0700

    Create vowellist.py

---
+++ vowellist.py
@@ -0,0 +1,9 @@
+sri=input("")
+vowels=['a','e','i','o','u']
+consonants=['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+if sri in vowels:
+  print("vowels")
+elif sri in consonants:
+  print("consonants")
+else:
+  print("invalid")
